0a69c5ab77599b0f8ffa63a4ec46deed50282505
kraigmansfield <Kraig.mansfield@gmail.com>
2023-03-17 09:52:26 -0700

    added registration form with REGEX

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Register from './Register';
+import Login from './Login'
 function App() {
   return (
     <main className="App">
--- src/Register.js
+++ src/Register.js
@@ -1,66 +1,189 @@
-import { useRef, useState, useEffect } from "react";
-import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from './api/axios;'
-
-
-const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-const REGISTER_URL = '/register';
-
 import React from 'react'
-import userEvent from "@testing-library/user-event";
+import { useRef, useState, useEffect } from 'react'
+import userEvent from '@testing-library/user-event'
+import axios from './api/axios'
 
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/
+const REGISTER_URL = '/register'
 
 const Register = () => {
-    const userRef = userRef();
-    const errRef = userRef();
+  const userRef = useRef()
+  const errRef = useRef()
 
-    const [user,setUser] = useState('');
-    const [validName,setValidName] = useState(false);
-    const [userFocus,setUserFocus] = useState(false);
+  const [user, setUser] = useState('')
+  const [validName, setValidName] = useState(false)
+  const [userFocus, setUserFocus] = useState(false)
 
-    const [pwd, setPwd] = useState('');
-    const [validPwd, setValidPwd] = useState(false);
-    const [pwdFocus, setPwdFocus] = useState(false);
+  const [email, setEmail] = useState('')
+  const [validEmail, setValidEmail] = useState(false)
+  const [emailFocus, setEmailFocus] = useState(false)
 
-    const [matchPwd, setMatchPwd] = useState('');
-    const [validMatch, setValidMatch] = useState(false);
-    const [matchFocus, setMatchFocus] = useState(false);
+  const [password, setPassword] = useState('')
+  const [passwordFocus, setPasswordFocus] = useState(false)
 
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+  const [matchPassword, setMatchPassword] = useState('')
+  const [validMatch, setValidMatch] = useState(false)
+  const [matchFocus, setMatchFocus] = useState(false)
 
-    useEffect(()=> {
-        userRef.current.focus();
-    }, [])
+  const [errMsg, setErrMsg] = useState('')
+  const [success, setSuccess] = useState(false)
 
-    useEffect(()=> {
-        setValidName(USER_REGEX.test(user));
-    }, [user])
+  useEffect(() => {
+    userRef.current.focus()
+  }, [])
 
-    useEffect(()=> {
-        setValidPwd(PWD_REGEX.test(pwd));
-        setValidMatch(pwd===matchPwd);
-    }, [user, pwd, matchPwd])
+  useEffect(() => {
+    setValidName(USER_REGEX.test(user))
+  }, [user])
 
-    useEffect(()=> {
-        setErrMsg('');
-    }, [user, pwd, matchPwd])
+  useEffect(() => {
+    setValidEmail((user))
+  }, [user])
 
+  useEffect(() => {
+    setValidMatch(password === matchPassword)
+  }, [user, password, matchPassword])
 
+  useEffect(() => {
+    setErrMsg('')
+  }, [user, password, matchPassword])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post(
+        REGISTER_URL,
+        JSON.stringify({ user, password }),
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          withCredentials: true,
+        },
+      )
+
+      setUser('');
+      setPassword('');
+      setMatchPassword('');
+
+    } catch (err) {
+        if (!err?.response) {
+            setErrMsg('No Server Response');
+        } else if (err.response?.status === 409) {
+            setErrMsg('Username Taken');
+        } else {
+            setErrMsg('Registration Failed')
+        }
+        errRef.current.focus();
+    }
+  }
   return (
     <section>
+      <p
+        ref={errRef}
+        className={errMsg ? 'errmsg' : 'offscreen'}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          required
+          aria-describedby="uidnote"
+          onFocus={() => setUserFocus(true)}
+          onBlur={() => setUserFocus(false)}
+        />
+        <p
+          id="uidnote"
+          className={
+            userFocus && user && !validName ? 'instructions' : 'offscreen'
+          }
+        >
+          4 to 24 characters.
+          <br />
+          Must begin with a letter.
+          <br />
+          Letters, numbers, underscores, hyphens allowed.
+        </p>
         
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          aria-describedby="uidnote"
+          onFocus={() => setUserFocus(true)}
+          onBlur={() => setUserFocus(false)}
+        />
+        <br></br>
+        <br></br>
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+          aria-describedby="pwdnote"
+          onFocus={() => setPasswordFocus(true)}
+          onBlur={() => setPasswordFocus(false)}
+        />
+        <p
+          id="pwdnote"
+          className={
+            passwordFocus ? 'instructions' : 'offscreen'
+          }
+        >
+          8 to 24 characters.
+          <br />
+          Must include uppercase and lowercase letters, a number and a special
+          character.
+          <br />
+          Allowed special characters: !@#$%
+        </p>
+
+        <label htmlFor="confirm_pwd">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirm_pwd"
+          onChange={(e) => setMatchPassword(e.target.value)}
+          value={matchPassword}
+          required
+          aria-describedby="confirmnote"
+          onFocus={() => setMatchFocus(true)}
+          onBlur={() => setMatchFocus(false)}
+        />
+        <p
+          id="confirmnote"
+          className={matchFocus && !validMatch ? 'instructions' : 'offscreen'}
+        >
+          Must match the first password input field.
+        </p>
+        <button>
+          Sign Up
+        </button>
+      </form>
+      <p>
+        Already registered?
+        <br />
+        <span className="line">
+          {/*put router link here*/}
+          <a href="#">Sign In</a>
+        </span>
+      </p>
     </section>
   )
 }
-
-
-
-
-
-
 
 export default Register
